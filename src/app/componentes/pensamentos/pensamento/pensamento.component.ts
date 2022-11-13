f86d51a1a011f070c28339519d49356311f3504a
Sidney Santos <sidney_ssantos@hotmail.com>
2022-11-13 14:29:47 -0300

    Angular 14: Aplique os conceitos

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() pensamento: Pensamento = {
+    conteudo: 'I Love Angular',
+    autoria: 'Day',
+    modelo: 'modelo3'
+  }
+
+  ngOnInit(): void {
+  }
+
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
+  }
+
+}
